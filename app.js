73002b8eaac44189b36bc6c5d1bc69d54893efae
Hozaifa Abdalla <abdallahozaifa19527@gmail.com>
2016-07-09 22:53:16 +0000

    first commit

---
+++ app.js
@@ -0,0 +1,59 @@
+/*****************************
+ *     SALAM SERVER          *
+ *****************************/
+/* global */
+
+/* Imports */
+var express = require('express');
+var bodyParser = require('body-parser');
+var fs = require('fs');
+var app = express();
+var root = __dirname;
+
+////////////
+/* CONFIG */
+////////////
+
+app.set('port', (process.env.PORT || 8080));
+
+//////////////
+/* WEBSITE */
+//////////////
+
+app.use('/assets', express.static(root + '/www/salam-dev/salam-site/assets'));
+app.use('/style.css' , express.static(root + '/www/salam-dev/salam-site/style.css'));
+app.use('/404.html' , express.static(root + '/www/salam-dev/salam-site/404.html'));
+app.use('/index.html' , express.static(root + '/www/salam-dev/salam-site/index.html'));
+app.use('/salam.css' , express.static(root + '/www/salam-dev/salam-site/salam.css'));
+
+
+// parse application/x-www-form-urlencoded 
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+// parse application/json 
+app.use(bodyParser.json());
+
+//////////////
+/* GET/POST */
+//////////////
+
+/* Main Page for Salam Site */
+app.get('/', function(req, res) {
+    /* Sends the index html page to the user */
+    fs.readFile('www/salam-dev/salam-site/index.html', 'utf8', function(err, data) {
+        if (!err) res.send(data);
+        else return console.log(err);
+    });
+});
+
+
+////////////
+/* LISTEN */
+////////////
+
+app.listen(app.get('port'), function() {
+    console.log('Sparta is running on port', app.get('port'));
+    console.log("https://spartaaaaaaaaaaaaa-abdallahozaifa.c9users.io/");
+});
